be3a6527c0e0e960c5879dbb285d6a86ed6565c4
fhernandez2583 <40375223+fhernandez2583@users.noreply.github.com>
2024-10-02 17:06:12 -0500

    removed first string, added two stings to the end, replace one string

--- SecretMessage.js
+++ SecretMessage.js
@@ -1,1 +1,9 @@
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+//console.log(secretMessage.length);
+secretMessage.pop();
+//console.log(secretMessage.length);
+secretMessage.push('to', 'Program');
+//console.log(secretMessage.length);
+secretMessage[7] = 'right';
+//console.log(secretMessage.length);
+//console.log(secretMessage);
