805f7a4b40450a17c101d2c3c55d567038953730
fhernandez2583 <40375223+fhernandez2583@users.noreply.github.com>
2024-10-03 18:35:21 -0500

    added loop to run through each character

--- WhaleTalk.js
+++ WhaleTalk.js
@@ -3,3 +3,8 @@ let input = "Hello Water World!";
 //Setup for output
 const vowels = ["a", "e", "i", "o", "u"];
 let resultArray = [];
+//console.log(input.length);
+for (let i = 0; i <= input.length; i++)
+    {
+      //console.log(i);
+    }
