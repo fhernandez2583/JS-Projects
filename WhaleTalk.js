82bceac4ba3c3248651e20af3769666df40b4ae6
fhernandez2583 <40375223+fhernandez2583@users.noreply.github.com>
2024-10-03 19:09:02 -0500

    Doubled 'e' and 'u'; joined the results into a string and logged results

--- WhaleTalk.js
+++ WhaleTalk.js
@@ -1,15 +1,22 @@
-//My phase to be translated
-let input = "Hello Water World!";
-//Setup for output
+let input = "turpentine and turtles";
 const vowels = ["a", "e", "i", "o", "u"];
 let resultArray = [];
 //console.log(input.length);
 for (let indexInput = 0; indexInput <= input.length; indexInput++) {
-    //console.log(indexInput);
-    for (let indexVowels = 0; indexVowels < vowels.length; indexVowels++) {
-      //console.log(indexVowels);
-      if (input[indexInput] === vowels[indexVowels]){
-        //console.log(input[indexInput])
-      }
+  //console.log(indexInput);
+  if (input[indexInput] === "e") {
+    resultArray.push(input[indexInput]);
+  }
+  if (input[indexInput] === "u") {
+    resultArray.push(input[indexInput]);
+  }
+  for (let indexVowels = 0; indexVowels < vowels.length; indexVowels++) {
+    //console.log(indexVowels);
+    if (input[indexInput] === vowels[indexVowels]) {
+      //console.log(input[indexInput])
+      resultArray.push(input[indexInput]);
     }
   }
+}
+let resultString = resultArray.join("").toUpperCase();
+console.log(resultString);
