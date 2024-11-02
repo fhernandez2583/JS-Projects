2938f828a2996f820a830957ca9c50234ed7e8c4
fhernandez2583 <40375223+fhernandez2583@users.noreply.github.com>
2024-11-01 19:34:34 -0500

    formated code

--- MealMaker.js
+++ MealMaker.js
@@ -1,17 +1,17 @@
 const menu = {
-    _meal: "",
-    _price: 0,
-  
-    set meal(mealToCheck) {
-      if (typeof mealToCheck === "string") {
-        return (this._meal = mealToCheck);
-      }
-    },
-    set price(priceToCheck) {
-      if (typeof priceToCheck === "number") {
-        return (this._price = priceToCheck);
-      }
-    },
+  _meal: "",
+  _price: 0,
+
+  set meal(mealToCheck) {
+    if (typeof mealToCheck === "string") {
+      return (this._meal = mealToCheck);
+    }
+  },
+  set price(priceToCheck) {
+    if (typeof priceToCheck === "number") {
+      return (this._price = priceToCheck);
+    }
+  },
   get todaysSpecial() {
     if (this._meal && this._price) {
       return `Today's Special is ${this._meal} for $${this._price}!`
@@ -19,11 +19,10 @@ const menu = {
       return `Meal or price was not set correctly!`
     }
   }
-  };
-  menu.meal = "pizza";
-  menu.price = 8;
-  console.log(menu.todaysSpecial);
-  //menu._meal = 0
-  //menu._price = 'pizza'
-  //console.log(menu);
-  
+};
+menu.meal = "pizza";
+menu.price = 8;
+console.log(menu.todaysSpecial);
+//menu._meal = 0
+//menu._price = 'pizza'
+//console.log(menu);
