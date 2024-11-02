8dbf5fbdb2bcd07df01285b6e0a67e1ca22f458e
fhernandez2583 <40375223+fhernandez2583@users.noreply.github.com>
2024-11-01 19:37:38 -0500

    added get method to players and games

--- TeamStats.js
+++ TeamStats.js
@@ -1,12 +1,19 @@
 const team = {
     _players: [
-        { firstName: "John", lastName: "Doe", age: 26 },
-        { firstName: "Sam", lastName: "Smith", age: 25 },
-        { firstName: "Tim", lastName: "Harris", age: 27 },
+      { firstName: "John", lastName: "Doe", age: 26 },
+      { firstName: "Sam", lastName: "Smith", age: 25 },
+      { firstName: "Tim", lastName: "Harris", age: 27 },
     ],
     _games: [
-        { opponent: "Ravens", teamPoints: 20, opponentPoints: 15 },
-        { opponent: "Dodgers", teamPoints: 10, opponentPoints: 5 },
-        { opponent: "Lions", teamPoints: 25, opponentPoints: 20 },
+      { opponent: "Ravens", teamPoints: 20, opponentPoints: 15 },
+      { opponent: "Dodgers", teamPoints: 10, opponentPoints: 5 },
+      { opponent: "Lions", teamPoints: 25, opponentPoints: 20 },
     ],
-};
+    get players() {
+      return this._players;
+    },
+    get games() {
+      return this._games;
+    },
+  };
+  
