8ff9fd75ca05d21c6bb9dffbcdd2fae83ee3e33d
fhernandez2583 <40375223+fhernandez2583@users.noreply.github.com>
2024-11-01 19:02:05 -0500

    added check for string

--- MealMaker.js
+++ MealMaker.js
@@ -1,10 +1,16 @@
 const menu = {
     _meal: '',
-    _price: 0
+    _price: 0,
+  
+    set meal(mealToCheck) {
+    if (typeof mealToCheck === 'string') {
+      return this.meal = mealToCheck
+    }
+  }
   };
   
-  menu._meal = 0
-  menu._price = 'Hello'
-  console.log(menu);
+  //menu._meal = 0
+  //menu._price = 'pizza'
+  //console.log(menu);
   
   
