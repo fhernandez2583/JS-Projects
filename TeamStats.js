76c0efc7d6aaf6f3948d513f5b8a57a888e8c81e
fhernandez2583 <40375223+fhernandez2583@users.noreply.github.com>
2024-11-01 19:47:25 -0500

    created player and game add option

--- TeamStats.js
+++ TeamStats.js
@@ -15,5 +15,28 @@ const team = {
     get games() {
       return this._games;
     },
+    addPlayer(newFirstName, newLastName, newAge){
+      let player = {
+        firstName: newFirstName,
+        lastName: newLastName,
+        age: newAge
+      };
+      this.players.push(player);
+    },
+    addGame(newOpponent, newTeamPoints, newOpponentPoints){
+      let game = {
+        opponent: newOpponent,
+        teamPoints: newTeamPoints,
+        opponentPoints: newOpponentPoints
+      };
+      this.games.push(game);
+    }
   };
+  
+  team.addPlayer('Bugs', 'Bunny', 76);
+  console.log(team.players);
+  team.addGame('Titans', 100, 98);
+  console.log(team.games);
+  
+  
   
