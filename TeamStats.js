bd0041ff1861901d70813632d47e39b6991ca9e3
fhernandez2583 <40375223+fhernandez2583@users.noreply.github.com>
2024-11-01 19:33:07 -0500

    Populating data

---
+++ TeamStats.js
@@ -0,0 +1,13 @@
+const team = {
+    _players: [
+      { firstName: "John", lastName: "Doe", age: 26 },
+      { firstName: "Sam", lastName: "Smith", age: 25 },
+      { firstName: "Tim", lastName: "Harris", age: 27 },
+    ],
+    _games: [
+      { opponent: "Ravens", teamPoints: 20, opponentPoints: 15 },
+      { opponent: "Dodgers", teamPoints: 10, opponentPoints: 5 },
+      { opponent: "Lions", teamPoints: 25, opponentPoints: 20 },
+    ],
+  };
+  
