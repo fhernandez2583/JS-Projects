95491331de95784836b4845da9e37b8e6cebd346
fhernandez2583 <40375223+fhernandez2583@users.noreply.github.com>
2024-11-01 19:09:15 -0500

    testing setters

--- MealMaker.js
+++ MealMaker.js
@@ -1,21 +1,22 @@
 const menu = {
-    _meal: '',
+    _meal: "",
     _price: 0,
   
     set meal(mealToCheck) {
-    if (typeof mealToCheck === 'string') {
-      return this._meal = mealToCheck
-    }
-  },
+      if (typeof mealToCheck === "string") {
+        return (this._meal = mealToCheck);
+      }
+    },
     set price(priceToCheck) {
-    if (typeof priceToCheck === 'string') {
-      return this._price = priceToCheck
-    }
-  }
+      if (typeof priceToCheck === "string") {
+        return (this._price = priceToCheck);
+      }
+    },
   };
-  
+  menu.meal = 8;
+  menu.price = "pizza";
+  console.log(menu);
   //menu._meal = 0
   //menu._price = 'pizza'
   //console.log(menu);
-  
   
